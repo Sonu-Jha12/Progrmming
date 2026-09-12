96ebf59e373304242074c41d352a10ea69c72aab
Sonu-jha12 <jhasonu8877206126@gmail.com>
2026-09-12 22:44:32 +0530

    Create Simple_Interest.cpp in which I calculate Simple Interest.

---
+++ CPP/Basics/Simple_Interest.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float P,R,T,SI;
+
+    cout<<"Enter Principal(P) : ";
+    cin>> P;
+
+    cout<<"Enter Rate(R) : ";
+    cin>> R;
+
+    cout<<"Enter Time(T) : ";
+    cin>> T;
+
+    SI = (P * R * T)/100;
+
+    cout<<"Simple Interest : "<<SI<<endl;
+
+    return 0;
+}
