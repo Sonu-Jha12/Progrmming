8da669b3c764a5260eff6de90fb2bfd1e82592f2
Sonu-jha12 <jhasonu8877206126@gmail.com>
2026-09-12 21:58:59 +0530

    create Area_Square.cpp In which We calculate Area of Square

---
+++ CPP/Basics/Area_Square.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float side,Area;
+
+    cout<<"Enter Side of Square : ";
+    cin>> side;
+
+    Area = side * side;
+
+    cout<<"Area of Sqaure is "<<Area<<endl;
+
+
+}
