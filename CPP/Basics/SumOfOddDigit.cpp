555f2d5a24fd6d2e256a8f228e85ae19abf3ea90
Sonu-jha12 <jhasonu8877206126@gmail.com>
2026-09-14 21:51:54 +0530

    Create SumOfOddDigit.cpp In which I calculate odd number digit in a number.

---
+++ CPP/Basics/SumOfOddDigit.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num, sum = 0;
+
+    cout<<"Enter Number : ";
+    cin>> num;
+
+    while(num > 0){
+        int digit = num % 10;
+        if(digit % 2 != 0){
+            sum += digit;
+        }
+        num /= 10;
+    }
+
+    cout<<"Sum of Digit in a Number is "<<sum<<endl;
+
+    return 0;
+}
