8669eb232feecefd2b8cea11a8e0c35b61e07da4
Sonu-jha12 <jhasonu8877206126@gmail.com>
2026-09-14 20:22:05 +0530

    Create PrintNumber.cpp In which we Print natural number 1 to n.

---
+++ CPP/Basics/PrintNumber.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+
+    cout<<"Enter n (1 to n) : ";
+    cin>> n;
+
+    for(int i=0; i<n; i++){
+        cout<<i+1<<endl;
+    }
+
+    cout<<endl;
+}
