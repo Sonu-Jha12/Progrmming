6d21dd8fcbe9e54132a6dd48c84305e45b7680a3
Sonu-jha12 <jhasonu8877206126@gmail.com>
2026-09-15 15:21:40 +0530

    Crate PrintNPrime.cpp In which I Print all Prime number upto N

---
+++ CPP/Basics/PrintNPrime.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+
+    cout<< "Enter Number : ";
+    cin>> num;
+
+    cout<< "Prime Number (2 to N) : ";
+
+    for(int i=2; i<=num; i++){
+        
+        int n = i;
+        bool isPrime = true;
+
+        if(n == 2){
+            isPrime = true;
+        }
+        else{
+            for(int j=2; j<n; j++){
+                if(n % j == 0){
+                    isPrime = false;
+                }
+            }
+        }
+        if(isPrime)
+            cout<<n<<" ";
+    }
+    cout<< endl;
+    return 0;
+
+}
