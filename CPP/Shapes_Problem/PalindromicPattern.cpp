a22babaecf571572b3e7845499646db54368f9e5
Sonu-jha12 <jhasonu8877206126@gmail.com>
2026-09-15 19:19:39 +0530

    Create PalindromicPattern.cpp In which I print Center Palindromic Pattern

--- CPP/Shapes_Problem/PalindromicPattern.cpp
+++ CPP/Shapes_Problem/PalindromicPattern.cpp
@@ -7,12 +7,19 @@ int main(){
     cin>> n;
 
     for(int i=1; i<=n; i++){
+        // Space
         for(int j=1; j<=n-i; j++){
-            cout<<i;
+            cout<<" ";
         }
-        for(int j=1; j<=n; j++){
-            cout<<"*";
+        // Left
+        for(int j=i; j>=1; j--){
+            cout<<j;
         }
+        // Right
+        for(int j=2; j<=i;j++){
+            cout<<j;
+        }
+
         cout<<endl;
     }
 
