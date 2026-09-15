e0dc1ad8878c298aa5d95bb29ed63842fda21970
Sonu-jha12 <jhasonu8877206126@gmail.com>
2026-09-15 16:37:43 +0530

    Create Triangle0_1.cpp In which I print Right Triangle 0_1

--- CPP/Shapes_Problem/Triangle0_1.cpp
+++ CPP/Shapes_Problem/Triangle0_1.cpp
@@ -2,18 +2,21 @@
 using namespace std;
 
 int main(){
-
     int n;
-
-    cout<<"Enter Number of * in a line : ";
+    cout<< "Enter n : ";
     cin>> n;
 
+    bool isNum = true;
+
     for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cout<<"*";
+        for(int j=0; j<=i; j++){
+
+            cout<<isNum;
+            
+            isNum = !isNum;
         }
         cout<<endl;
     }
+
     return 0;
-    
 }
