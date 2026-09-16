2f79b5076980dcd773fa4775ab52bf74a002b618
Sonu-jha12 <jhasonu8877206126@gmail.com>
2026-09-16 19:05:13 +0530

    Create Diamond.cpp In which I print Diamond Shapes

--- CPP/Shapes_Problem/Diamond.cpp
+++ CPP/Shapes_Problem/Diamond.cpp
@@ -12,28 +12,20 @@ int main(){
         for(int j = 1; j <= n - i; j++){
             cout<<" ";
         }
-        // Left
-        for(int j = 1; j <= i; j++){
-            cout<<"*";
-        }
-        // Right
-        for(int j = 2; j <= i; j++){
+        // Star
+        for(int j = 1; j <= 2*i-1; j++){
             cout<<"*";
         }
         cout<<endl;
     }
     //Lower
-    for(int i = 1; i <= n; i++){
+    for(int i = n; i >= 1; i--){
         // Space
-        for(int j = 1; j < i; j++){
+        for(int j = 1; j <= n-i; j++){
             cout<<" ";
         }
-        // Left
-        for(int j = n; j >= i; j--){
-            cout<<"*";
-        }
-        // Right
-        for(int j = n; j > i; j--){
+        // Star
+        for(int j = 1; j <= 2*i-1; j++){
             cout<<"*";
         }
         cout<<endl;
