dd7aced0d08b856d8b587ebb021b789968adf8bc
Sonu-jha12 <jhasonu8877206126@gmail.com>
2026-09-17 18:38:41 +0530

    Create APlusBSquare.cpp In which I Print (A + B)^2

---
+++ CPP/Basics/APlusBSquare.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    
+    cout<<"Enter a : ";
+    cin>> a;
+    cout<<"Enter b : ";
+    cin>> b;
+
+    cout<<a<<"^2 + "<<b<<"^2 + 2*"<<a<<"*"<<b<<endl;
+
+    return 0;
+}
