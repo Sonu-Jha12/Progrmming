48c424e20189407286df95b9df77b97af7b23bf5
Sonu-jha12 <jhasonu8877206126@gmail.com>
2026-09-17 18:27:58 +0530

    Create Palindrome.cpp In which I check is given number Palindrome or not?

---
+++ CPP/Basics/Palindrome.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+
+    cout<< "Enter Number : ";
+    cin>> num;
+
+    int n = num;
+    int result = 0;
+
+    while(num>0){
+        int rem = num % 10;
+        result = result * 10 + rem;
+        num = num / 10;
+    }
+    if(result == n){
+        cout<<n<<" is a Palindrome Number."<<endl;
+    }
+
+    return 0;
+}
